af485300d6c728cf989f36a0b287c3acf52acadf
Tywacol <tylerdurdenleg@gmail.com>
2020-02-04 09:13:24 +0100

    import project into idea

--- composants/And.java
+++ composants/And.java
@@ -1,3 +1,8 @@
 public class And extends Porte2Entrees {
-	
+	public boolean getEtat() throws NonConnecteException {
+		if (super.in1 == null || super.in2 == null) {
+			throw new NonConnecteException();
+		}
+		return super.in1 && super.in2;
+	}
 }
